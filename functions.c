68c64188680f49a9181a0fd4d4a792d69b5a933b
Shatha <s.alghamdiwork@gmail.com>
2026-08-04 15:37:30 +0300

    Divide the tasks and created the file structure

---
+++ functions.c
@@ -0,0 +1,22 @@
+// c > single character shatha 
+//s >  string of characters
+//% >  percent sign 
+
+// d > 
+//i
+/*
+Shatha tasks
+Main.h file
++ those functions 
+c > single character  
+s >  string of characters
+% >  percent sign 
+
+flow chart as addition 
+
+
+Arwa 
+d > prinnt decimal (base 10) number 
+i > print integer in base 10
+README file 
+*/
