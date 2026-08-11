d5629ebd760559acd522d035164b93bfd51ca137
Shatha <s.alghamdiwork@gmail.com>
2026-08-11 20:05:51 +0300

    fixing number function

--- functions.c
+++ functions.c
@@ -1,18 +1,3 @@
-/*
--- Shatha's tasks
-Main.h file
-+ those functions 
-c > single character  
-s >  string of characters
-% >  percent sign 
-flow chart as addition 
--- Arwa's
-d > prinnt decimal (base 10) number 
-i > print integer in base 10
-README file 
-*/
-
-#include <stdio.h>
 #include "main.h"
 
 /**
